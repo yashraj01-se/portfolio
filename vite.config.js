c061b3477ebed1a58f1bc1c0160bc281c1df2e94
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2025-10-23 00:00:04 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react', 
-        'react-dom'
-      ],
-    },
-  },
 })
