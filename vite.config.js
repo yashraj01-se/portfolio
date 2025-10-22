0c9d87588f314bfad3285043f1b5c4d54abc4078
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2025-10-22 23:43:41 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { builtinModules } from 'module'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
-      // ✅ Externalize Node core modules + Tailwind plugin
-      external: [
-        '@tailwindcss/vite',
-        ...builtinModules,
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      fs: false,
-      path: false,
-      os: false,
-      url: false,
-      crypto: false,
+      onwarn(warning, warn) {
+        // 👇  print every warning in full
+        console.warn('ROLLUP WARNING:', warning)
+        warn(warning)
+      },
     },
   },
 })
