2782ed85984b6eb4a36d5e7d8f30775a8e830bca
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2025-10-22 23:37:28 +0530

    fix: externalized @tailwindcss/vite for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      // ✅ Tell Rollup not to try bundling the Tailwind plugin
+      external: ['@tailwindcss/vite'],
+    },
+  },
 })
